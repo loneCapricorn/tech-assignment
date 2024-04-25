a1431a63f35ddb12e5c23d0757716bbe2b786c8a
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-25 14:54:15 +0300

    Update layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import SideMenu from "../components/SideMenu";
 
 const RootLayout = () => {
   return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
+    <div className="grid grid-cols-6">
+      <aside className="max-md:col-span-6 md:col-span-2 md:h-screen lg:col-span-1">
+        <SideMenu />
+      </aside>
+
+      <div className="max-md:col-span-6 md:col-span-4 lg:col-span-5">
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </div>
   );
 };
