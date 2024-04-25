da770fbe1424ecbaa3c6fdb47274d0d4482e3379
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-25 17:26:14 +0300

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+const loginURL = "https://0wzm4.wiremockapi.cloud/login";
+const orderHistoryURL = "https://0wzm4.wiremockapi.cloud/orders";
+
+const sideMenuItems = [
+  {
+    label: "Dashboard",
+    path: "/",
+  },
+  {
+    label: "Login",
+    path: "/login",
+  },
+  {
+    label: "Order history",
+    path: "/order-history",
+  },
+];
+
+export { loginURL, orderHistoryURL, sideMenuItems };
