62182c969f10b572b9494338307e8ad5bb6d4c0a
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-25 17:26:35 +0300

    Update sideMenu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom";
+import { sideMenuItems } from "../constants";
+import { v4 as uniqueId } from "uuid";
 
 const SideMenu = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to={"/"}>Dashboard</NavLink>
-      </li>
-      <li>
-        <NavLink to={"/login"}>Login</NavLink>
-      </li>
-      <li>
-        <NavLink to={"/order-history"}>Order history</NavLink>
-      </li>
+    <ul className="flex md:flex-col md:h-52 items-center justify-around py-10">
+      {sideMenuItems.map(({ label, path }) => (
+        <li key={uniqueId()}>
+          <NavLink to={path} className="font-montserrat text-xl md:text-2xl">
+            {label}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 };
