3b2a7d7aeadd718cb5251dfeef6e0a8c85d8a649
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-26 17:15:29 +0300

    Add new color in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
       colors: {
         primary: "#242424",
         "slate-gray": "#6D6D6D",
+        "white-400": "rgba(255, 255, 255, 0.80)",
       },
       boxShadow: {
         "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
