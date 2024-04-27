22203a4f367ce6e01af2cda9e56073fabe4b7966
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-27 10:47:10 +0300

    Add hover effects

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -2,14 +2,14 @@ import PropTypes from "prop-types";
 
 const OrderCard = ({ product, quantity, priceForOne, timestamp }) => {
   return (
-    <li className="flex-1 sm:w-[350px] rounded-2xl shadow-3xl px-10 py-12">
+    <li className="flex-1 sm:w-[350px] rounded-2xl shadow-3xl px-10 py-12 transition duration-500 hover:scale-105">
       <h2 className="font-palanquin text-3xl leading-tight font-bold">
         {product}
       </h2>
 
       <div className="flex flex-col gap-2 mt-5 break-words font-montserrat text-lg leading-none text-slate-gray">
         <p>Quantity: {quantity}</p>
-        <p>Price: {priceForOne}</p>
+        <p>Price: ${priceForOne}</p>
         <p>Date: {new Date(timestamp).toLocaleDateString()}</p>
       </div>
     </li>
