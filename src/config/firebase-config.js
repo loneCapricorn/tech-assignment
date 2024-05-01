a9b4b43d439252ea9f181df5ba9c41344c989de1
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-05-01 20:45:20 +0300

    Add firebase (realtime db)

---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCdXvwVo_KAEJj-QV4EvyOJQ3sSwB4ZSs",
+  authDomain: "tech-assignment-8c7aa.firebaseapp.com",
+  projectId: "tech-assignment-8c7aa",
+  storageBucket: "tech-assignment-8c7aa.appspot.com",
+  messagingSenderId: "16928893138",
+  appId: "1:16928893138:web:3da11385b0e0c6d995650b",
+  databaseURL:
+    "https://tech-assignment-8c7aa-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
