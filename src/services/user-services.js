9c75c082f7cd7a2af626177e61fff35e11a2c920
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-05-02 22:54:34 +0300

    Add user services

---
+++ src/services/user-services.js
@@ -0,0 +1,21 @@
+import { get, set, ref, query, equalTo, orderByChild } from "firebase/database";
+import { db } from "../config/firebase-config";
+
+const getUserByHandle = (handle) => {
+  return get(ref(db, `users/${handle}`));
+};
+
+const createUserHandle = (handle, uid, email) => {
+  return set(ref(db, `users/${handle}`), {
+    handle,
+    uid,
+    email,
+    createdOn: new Date(),
+  });
+};
+
+const getUserData = (uid) => {
+  return get(query(ref(db, "users"), orderByChild("uid"), equalTo(uid)));
+};
+
+export { getUserByHandle, createUserHandle, getUserData };
