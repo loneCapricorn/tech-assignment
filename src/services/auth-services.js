86d9fd645a3fa302c3a7c0d3f164f5a1b2cb1a05
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-05-02 22:54:07 +0300

    Add authentication services

---
+++ src/services/auth-services.js
@@ -0,0 +1,20 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../config/firebase-config";
+
+const registerUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const logoutUser = () => {
+  return signOut(auth);
+};
+
+export { registerUser, loginUser, logoutUser };
