c78bdb4485bc49355f055a71a49c09b42225bc68
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-05-03 16:24:32 +0300

    Add sign-up logic and Context provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./config/firebase-config.js";
+import { getUserData } from "./services/user-services.js";
+import AppContext from "./providers/AppContext.js";
 
 // layouts and pages
 import RootLayout from "./layouts/RootLayout";
 import MainDashboard from "./pages/MainDashboard";
 import NotFound from "./pages/NotFound";
-import LoginPage from "./pages/LoginPage";
+import SignIn from "./pages/SignIn.jsx";
 import OrderHistory from "./pages/OrderHistory";
 import LandingPage from "./pages/LandingPage";
+import SignUp from "./pages/SignUp.jsx";
 
 // router and routes
 const router = createBrowserRouter(
@@ -21,7 +27,8 @@ const router = createBrowserRouter(
       <Route index element={<LandingPage />} />
       <Route path="*" element={<NotFound />} />
       <Route path="dashboard" element={<MainDashboard />} />
-      <Route path="login" element={<LoginPage />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route path="sign-up" element={<SignUp />} />
       <Route path="order-history" element={<OrderHistory />} />
     </Route>
   )
@@ -30,9 +37,36 @@ const router = createBrowserRouter(
 const queryClient = new QueryClient();
 
 const App = () => {
+  const [context, setContext] = useState({ user: null, userData: null });
+  const [user] = useAuthState(auth);
+
+  // update the user in the app state to match the one retrieved from the hook above
+  if (context.user !== user) {
+    setContext({ user });
+  }
+
+  useEffect(() => {
+    if (user === null) return;
+
+    getUserData(user.uid)
+      .then((snapshot) => {
+        if (!snapshot.exists()) {
+          throw new Error("Something went wrong!");
+        }
+
+        setContext({
+          ...context,
+          userData: snapshot.val()[Object.keys(snapshot.val())[0]],
+        });
+      })
+      .catch((e) => alert(e.message));
+  }, [user]);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AppContext.Provider value={{ ...context, setContext }}>
+        <RouterProvider router={router} />
+      </AppContext.Provider>
     </QueryClientProvider>
   );
 };
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,101 @@
+import { Form, useNavigate } from "react-router-dom";
+import { FORM_LOGO } from "../constants";
+
+// context
+import { useContext } from "react";
+import AppContext from "../providers/AppContext";
+
+// services
+import { createUserHandle, getUserByHandle } from "../services/user-services";
+import { registerUser } from "../services/auth-services";
+
+// icons
+import { CiUser } from "react-icons/ci";
+import { MdAlternateEmail } from "react-icons/md";
+import { CiLock } from "react-icons/ci";
+
+const SignUp = () => {
+  const { setContext } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const username = event.target.elements.username.value;
+    const email = event.target.elements.email.value;
+    const pass = event.target.elements.password.value;
+    const user = await getUserByHandle(username);
+
+    if (user.exists()) {
+      alert(`Username @${username} has already been taken!`);
+      return;
+    }
+
+    try {
+      const credentials = await registerUser(email, pass);
+      await createUserHandle(username, credentials.user.uid, email);
+      setContext({
+        user: credentials.user,
+        userData: null,
+      });
+      alert("Congratulations! Your account has been successfully created!");
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <main className="relative h-[92vh] bg-[#ecf0f3]">
+      <Form
+        onSubmit={handleSubmit}
+        className="absolute-position-center form-element"
+      >
+        <div className="flex gap-5 flex-col items-center mb-16">
+          <img
+            src={FORM_LOGO}
+            alt="form-logo"
+            className="w-24 h-24 rounded-[50%]"
+          />
+          <h2 className="text-[#555] text-3xl tracking-wider font-medium">
+            Sign up
+          </h2>
+        </div>
+        <div className="w-full p-1">
+          <div className="input-container">
+            <CiUser className="text-2xl text-[#999] mx-3" />
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              className="input-field"
+            />
+          </div>
+          <div className="input-container">
+            <MdAlternateEmail className="text-2xl text-[#999] mx-3" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="input-field"
+            />
+          </div>
+          <div className="input-container">
+            <CiLock className="text-2xl text-[#999] mx-3" />
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input-field"
+            />
+          </div>
+        </div>
+        <button className="outline-none border-none cursor-pointer w-full h-14 rounded-3xl text-xl font-bold text-white bg-[#44403c] shadow-button hover:bg-[#000]">
+          Sign up
+        </button>
+      </Form>
+    </main>
+  );
+};
+
+export default SignUp;
