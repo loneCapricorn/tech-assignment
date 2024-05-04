7ec27949aadd1f11aecdec83c6e245a70cc89b01
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-05-04 15:52:42 +0300

    Add logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,32 @@
-import PropTypes from "prop-types";
-
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { navbarItems } from "../constants";
 import { v4 as uniqueId } from "uuid";
+import { logoutUser } from "../services/auth-services";
 
-const Navbar = ({ changeHeader = () => {} }) => {
+const Navbar = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className="flex items-center justify-around py-4 max-sm:hidden">
       {navbarItems.map(({ label, path }) => (
         <NavLink
           key={uniqueId()}
           to={path}
-          onClick={() => {
-            changeHeader(label);
-          }}
-          className="font-montserrat"
+          className={`font-montserrat hover:text-sky-400 ${
+            pathname === path ? "text-sky-400 border-b-2" : ""
+          }`}
         >
           {label}
         </NavLink>
       ))}
+      <h2
+        className="text-center py-2 px-3 rounded-md w-fit text-lg hover:bg-slate-200 active:bg-slate-400 cursor-pointer"
+        onClick={logoutUser}
+      >
+        Sign out
+      </h2>
     </nav>
   );
-};
-
-Navbar.propTypes = {
-  changeHeader: PropTypes.func.isRequired,
 };
 
 export default Navbar;
